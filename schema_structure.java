6b603c3d65e3451aec765b8d97baf3ce5888c723
nikunj34 <48507843+nikunj34@users.noreply.github.com>
2019-06-20 13:32:20 +0530

    Add files via upload
    Just structure

---
+++ schema_structure.java
@@ -0,0 +1,32 @@
+﻿
+// Structure of all classes
+
+
+public class Verb {
+    String name,type;
+}
+public class Adjective{
+    String name;
+    int level;
+    int type;//general or specific
+}
+public class Prepositon{
+    
+}
+public class Column{
+    String name,postag,type,vartype;
+    ArrayList<String> prefix = new ArrayList<String>();
+    ArrayList<String> postfix = new ArrayList<String>();
+    ArrayList<String> synonym = new ArrayList<String>();
+    ArrayList<Adjective> adjectives = new ArrayList<Adjective>();
+    ArrayList<Verb> verbs = new ArrayList<Verb>();
+    ArrayList<Preposition> prepositions = new ArrayList<Preposition>();
+}
+public class Table{
+    Column primary_key = new Column();
+    ArrayList<Column> columns = new ArrayList<Column>();
+}
+public class Schema{
+    ArrayList<Table> tables = new ArrayList<Table>();
+    // heirarchy
+}  
