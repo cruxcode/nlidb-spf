2398f3f42c2490e943d56f7fd8f925e80fe81063
nikunj34 <48507843+nikunj34@users.noreply.github.com>
2019-06-20 12:46:13 +0530

    Add files via upload

---
+++ pred.java
@@ -0,0 +1,32 @@
+﻿import java.util.*;
+public class MyClass {
+    public static Dictionary pred = new Hashtable();
+    public static void predicate(String name, String ptype, String ctype){
+        List<String> funForm = new ArrayList<String>();
+        String fun = "";
+        fun += name + ":<" + symb.get(ptype) + ",t>";
+        System.out.println(fun);
+        funForm.add(fun);
+        fun = "";
+        fun += name + ":<" + symb.get(ptype) + "," + symb.get(ctype) + ">";
+        System.out.println(fun);
+        funForm.add(fun);
+        fun = "";
+        fun += name + ":<" + symb.get(ptype) + ",<" + symb.get(ptype) + ",t>>";
+        System.out.println(fun);
+        funForm.add(fun);
+        fun = "";
+        fun += name + ":<t,<" + symb.get(ptype) + "," + symb.get(ctype) + ">>";
+        System.out.println(fun);
+        funForm.add(fun);
+        pred.put(name,funForm);
+        
+    }
+    public static void main(String args[]) {
+        int x=10;
+        int y=25;
+        int z=x+y;
+
+        System.out.println("Sum of x+y = " + z);
+    }
+}
