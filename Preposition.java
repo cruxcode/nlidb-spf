b2d2aad36c2c5f1750a2dd0ee760b546769590a1
nikunj34 <48507843+nikunj34@users.noreply.github.com>
2019-06-21 11:47:18 +0530

    Delete Preposition.java

--- Preposition.java
+++
@@ -1,9 +0,0 @@
-package xml_reader;
-
-public class Preposition implements Cloneable {
-	String name,level,type;
-	public Object clone()throws CloneNotSupportedException{  
-		return super.clone();  
-		}
-	
-}
