420599ec143265a88d700d4cfae28d2d8c626cf1
nikunj34 <48507843+nikunj34@users.noreply.github.com>
2019-06-21 11:47:45 +0530

    Delete Table.java

--- Table.java
+++
@@ -1,9 +0,0 @@
-package xml_reader;
-import java.util.*;
-public class Table implements Cloneable {
-	//Column primary_key = new Column();
-    ArrayList<Column> columns = new ArrayList<Column>(100);
-    public Object clone()throws CloneNotSupportedException{  
-    	return super.clone();  
-    	}
-}
