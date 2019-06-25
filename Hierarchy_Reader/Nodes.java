0de487fb23ca984cb893e57aaa7e3d83bc203433
nikunj34 <48507843+nikunj34@users.noreply.github.com>
2019-06-25 22:51:10 +0530

    Update Nodes.java

--- Hierarchy_Reader/Nodes.java
+++ Hierarchy_Reader/Nodes.java
@@ -5,12 +5,13 @@ public class Nodes {
 	String name,symbol;
 	Nodes parent = null;
 	ArrayList<Nodes> children = new ArrayList<Nodes>();
-	public Nodes(String name, Nodes parent) {
+	public Nodes(String name, String symbol, Nodes parent) {
 		this.name = name;
 		this.parent = parent;
+		this.symbol = symbol;
 	}
 	public Nodes() {}
-	public void add_child(String cname) {
-		children.add(new Nodes(cname,this));
+	public void add_child(String cname, String symbol) {
+		children.add(new Nodes(cname,symbol,this));
 	}
 }
