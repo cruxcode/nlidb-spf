612a8d6f8db41c3da735e40e9e6fe53315200201
nikunj34 <48507843+nikunj34@users.noreply.github.com>
2019-07-12 13:51:18 +0530

    Add files via upload

---
+++ Documentation/Code/Generator.java
@@ -0,0 +1,428 @@
+package ccg;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Generator {
+	public String lastsymb = "";
+	
+	public static void generate_file(String FileName, ArrayList<String> arr) {
+		try {
+			FileGenerator.list_to_file(FileName, arr);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String nextPerm(String last) {
+		
+		String next = "";
+		
+		int carry = 1;
+		for(int i=last.length()-1; i>=0; i--) {
+			int num = last.charAt(i)-96+carry;
+			carry = num/26;
+			next = (char)(num%26 + 96) + next;
+		}
+		if(carry != 0) {
+			next = (char)(carry + 96) + next;
+		}
+
+        if(next.equals("t"))
+			next = "v";
+		if(next.equals("e"))
+			next = "f";
+		if(next.equals("i"))
+			next = "j";
+		if(next.equals("c"))
+			next = "d";
+		if(next.equals("s"))
+			next = "u";
+		if(next.equals("r"))
+			next = "w";
+		
+		lastsymb = next;
+		//System.out.println("aa "+lastsymb);
+		
+		return next;
+	}
+	public void predicate(Schema schema, Predicate pred_file)
+	{
+		ArrayList<Relation> RelationList = schema.RelationList;
+		Dictionary TrackType = schema.TrackType;
+		Dictionary TrackColumn = schema.TrackColumn;
+		Dictionary TrackConstant = schema.TrackConstant;
+		String last = "";
+		
+		for(int i=0; i<RelationList.size(); i++) {
+			
+			ArrayList<Column> ColumnList = RelationList.get(i).ColumnList;
+			Column PrimaryDetails = RelationList.get(i).PrimaryDetails;
+			
+			
+			String PVarType = PrimaryDetails.VarType;
+			String PName = PrimaryDetails.Name;
+			String PType = PrimaryDetails.Type;
+			
+			String ptype = "";
+			if(TrackType.get(PType)!=null) {
+				ptype = (String) TrackType.get(PType);
+			}
+			else {
+				ptype = nextPerm(last);
+			}
+			last = ptype;
+			
+			TrackColumn.put(PName, ptype);
+			TrackType.put(PType, ptype);
+			
+			pred_file.PredGenerator(PName, ptype, ptype, "category");
+			
+			for(int j=0; j<ColumnList.size(); j++) {
+				String VarType = ColumnList.get(j).VarType;
+				String Name = ColumnList.get(j).Name;
+				if(Name.equals(PName))
+					continue;
+				String Type = ColumnList.get(j).Type;
+				
+				if(VarType.equals("category")) {
+					//System.out.println(VarType);
+					String ctype = "";
+					if(TrackType.get(Type)!=null) {
+						ctype = (String) TrackType.get(Type);
+					}
+					else {
+						ctype = nextPerm(last);
+					}
+					last = ctype;
+					
+					TrackColumn.put(Name, ctype);
+					TrackType.put(Type, ctype);
+					
+					pred_file.PredGenerator(Name, ctype, ptype, "category");
+				}
+				
+				else {
+					String  ctype = "i";
+					TrackColumn.put(Name, ctype);
+					TrackType.put(Type, ctype);
+					
+					pred_file.PredGenerator(Name, ctype, ptype, "numeric");
+				}
+				
+			}
+		}
+		
+		schema.TrackColumn = TrackColumn;
+		schema.TrackType = TrackType;
+		schema.TrackConstant = TrackConstant;
+	}
+
+
+
+	public void constant(Schema schema, Constant const_file) {
+		ArrayList<Relation> RelationList = schema.RelationList;
+		Dictionary TrackType = schema.TrackType;
+		Dictionary TrackColumn = schema.TrackColumn;
+		Dictionary TrackConstant = schema.TrackConstant;
+		
+		for(int i=0; i<RelationList.size(); i++) {
+			
+			ArrayList<Column> ColumnList = RelationList.get(i).ColumnList;
+			Column PrimaryDetails = RelationList.get(i).PrimaryDetails;
+			
+			
+			for(int j=0; j<ColumnList.size(); j++) {
+				
+				ArrayList<String> ValueList = ColumnList.get(j).ValueList;
+				ArrayList<String> Prefix = ColumnList.get(j).Prefix;
+				ArrayList<String> Postfix = ColumnList.get(j).Postfix;
+				String Name = ColumnList.get(j).Name;
+				
+				for(int k=0; k<ValueList.size(); k++) {
+					
+					String ctype = (String)TrackColumn.get(Name);
+					if(TrackConstant.get(ValueList.get(k)) == null)
+					{
+						const_file.ConstGenerator(ValueList.get(k), ctype);
+						TrackConstant.put(ValueList.get(k), ctype);
+					}
+					
+					else
+					{
+						String comm_ancc = Hierar.CommonAncestor(ctype, (String)TrackConstant.get(ValueList.get(k)));
+						const_file.ConstGenerator(ValueList.get(k), ctype);
+						TrackConstant.put(ValueList.get(k), ctype);
+					}
+					
+					for(int l=0; l<Prefix.size(); l++) {
+						String prefix = Prefix.get(l) + " " + ValueList.get(l);
+						prefix = prefix.replace(" ", "_");
+						
+						if(TrackConstant.get(prefix) == null)
+						{
+							const_file.ConstGenerator(prefix, ctype);
+							TrackConstant.put(prefix, ctype);
+						}
+						
+						else
+						{
+							String comm_ancc = Hierar.CommonAncestor(ctype, (String)TrackConstant.get(prefix));
+							const_file.ConstGenerator(prefix, ctype);
+							TrackConstant.put(prefix, ctype);
+						}
+					}
+					
+					for(int l=0; l<Postfix.size(); l++) {
+						String postfix = ValueList.get(l) + " " + Postfix.get(l);
+						postfix = postfix.replace(" ", "_");
+						
+						if(TrackConstant.get(postfix) == null)
+						{
+							const_file.ConstGenerator(postfix, ctype);
+							TrackConstant.put(postfix, ctype);
+						}
+						
+						else
+						{
+							String comm_ancc = Hierar.CommonAncestor(ctype, (String)TrackConstant.get(postfix));
+							const_file.ConstGenerator(postfix, ctype);
+							TrackConstant.put(postfix, ctype);
+						}
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		schema.TrackColumn = TrackColumn;
+		schema.TrackType = TrackType;
+		schema.TrackConstant = TrackConstant;
+	}
+
+
+
+
+	public void nplist(Schema schema, NpList np_file) {
+		ArrayList<Relation> RelationList = schema.RelationList;
+		Dictionary TrackType = schema.TrackType;
+		Dictionary TrackColumn = schema.TrackColumn;
+		Dictionary TrackConstant = schema.TrackConstant;
+		
+		for(int i=0; i<RelationList.size(); i++) {
+			
+			ArrayList<Column> ColumnList = RelationList.get(i).ColumnList;
+			Column PrimaryDetails = RelationList.get(i).PrimaryDetails;
+			String ptype = TrackType.get(PrimaryDetails.Type).toString();
+			
+			for(int j=0; j<ColumnList.size(); j++) {
+				
+				ArrayList<String> ValueList = ColumnList.get(j).ValueList;
+				ArrayList<String> Prefix = ColumnList.get(j).Prefix;
+				ArrayList<String> Postfix = ColumnList.get(j).Postfix;
+				String Name = ColumnList.get(j).Name;
+				String PosTag = ColumnList.get(j).PosTag;
+				
+				for(int k=0; k<ValueList.size(); k++) {
+					
+					String ctype = (String)TrackConstant.get(ValueList.get(k));
+					np_file.NpGenerator(ValueList.get(k), ctype, ValueList.get(k), PosTag,ptype);
+					
+					for(int l=0; l<Prefix.size(); l++) {
+						String prefix = Prefix.get(l) + " " + ValueList.get(l);
+						
+						String org_prefix = prefix;
+						prefix = prefix.replace(' ', '_');
+						ctype = (String)TrackConstant.get(prefix);
+						prefix = org_prefix;
+						
+						np_file.NpGenerator(prefix, ctype, ValueList.get(l), PosTag,ptype);
+					}
+					
+					for(int l=0; l<Postfix.size(); l++) {
+						String postfix = ValueList.get(l) + " " + Postfix.get(l);
+
+						String org_postfix = postfix;
+						postfix = postfix.replace(' ', '_');
+						ctype = (String)TrackConstant.get(postfix);
+						postfix = org_postfix;
+						
+						np_file.NpGenerator(postfix, ctype, ValueList.get(l), PosTag,ptype);
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		schema.TrackColumn = TrackColumn;
+		schema.TrackType = TrackType;
+		schema.TrackConstant = TrackConstant;
+	}
+	
+	
+	public void seed_verb(Schema schema, Seed seed_file, Predicate pred_file){
+	    ArrayList<Relation> RelationList = schema.RelationList;
+		Dictionary TrackType = schema.TrackType;
+		Dictionary TrackColumn = schema.TrackColumn;
+		Dictionary TrackConstant = schema.TrackConstant;
+		
+		for(int i=0; i<RelationList.size(); i++){
+		    
+		    ArrayList<Column> ColumnList = RelationList.get(i).ColumnList;
+			Column PrimaryDetails = RelationList.get(i).PrimaryDetails;
+			
+			for(int j=0; j<ColumnList.size(); j++){
+			    
+			    ArrayList<Verb> VerbList = ColumnList.get(j).VerbList;
+			    String ColName = ColumnList.get(j).Name;
+			    String VarType = ColumnList.get(j).VarType;
+			    
+			    for(int k=0; k<VerbList.size(); k++){
+			        
+			        String VerbName = VerbList.get(k).VerbName;
+			        String VerbType = VerbList.get(k).VerbType;
+			        
+			        if(VarType.equals("category"))
+			        	seed_file.VerbGenerator(VerbName, VerbType, ColName, pred_file.pred_dict.get(ColName).get(1), pred_file.pred_dict.get(ColName).get(2));
+			        
+			        else
+			        	seed_file.VerbGenerator(VerbName, VerbType, ColName, pred_file.pred_dict.get(ColName).get(0), pred_file.pred_dict.get(ColName).get(1));
+			        
+			    }
+			}
+		}
+		
+		schema.TrackColumn = TrackColumn;
+		schema.TrackType = TrackType;
+		schema.TrackConstant = TrackConstant;
+	}
+	public void Adjective_seed(Schema s, Seed seed_file, Predicate pred_file) {
+		String s1,s2,s3,s4,s5;
+		for(int i=0;i<s.RelationList.size();i++) {
+			for(int j=0;j<s.RelationList.get(i).ColumnList.size();j++) {
+				for(int k=0;k<s.RelationList.get(i).ColumnList.get(j).AdjectiveList.size();k++) {
+					s1 = s.RelationList.get(i).ColumnList.get(j).AdjectiveList.get(k).name;
+					s2 = s.RelationList.get(i).ColumnList.get(j).AdjectiveList.get(k).level;
+					s3 = (pred_file.pred_dict.get(s.RelationList.get(i).ColumnList.get(j).Name)).get(0);
+					s4 = s.RelationList.get(i).ColumnList.get(j).AdjectiveList.get(k).type;
+					//s5 = s.RelationList.get(i).ColumnList.get(j).AdjectiveList.get(k).adjtype;
+					s5="1";
+					if(s4.equals("general")) {
+						if(s5.equals("1")) {
+							seed_file.Adjectives(s1,s2,s3,">:<i,<i,t>>","argmax:<<e,t>,<<e,i>,e>>");
+							seed_file.Adjectives(s1,s2,">:<i,<i,t>>","argmax:<<e,t>,<<e,i>,e>>");
+						}else {
+							seed_file.Adjectives(s1,s2,s3,"<:<i,<i,t>>","argmin:<<e,t>,<<e,i>,e>>");
+							seed_file.Adjectives(s1,s2,"<:<i,<i,t>>","argmin:<<e,t>,<<e,i>,e>>");
+						}
+					}else {
+						if(s5.equals("1")) {
+							seed_file.Adjectives(s1,s2,s3,">:<i,<i,t>>","argmax:<<e,t>,<<e,i>,e>>");
+						}else {
+							seed_file.Adjectives(s1,s2,s3,"<:<i,<i,t>>","argmin:<<e,t>,<<e,i>,e>>");
+						}
+					}
+				}
+			}
+		}
+	}
+	public void Column_Seed(Schema s, Seed seed_file, Predicate pred_file) {
+		String s1,s2,s3;
+		ArrayList<String> s4 = new ArrayList<String>();
+		for(int i=0;i<s.RelationList.size();i++) {
+			for(int j=0;j<s.RelationList.get(i).ColumnList.size();j++) {
+				s4.clear();
+				s1 = s.RelationList.get(i).ColumnList.get(j).Name;
+				s2 = s.RelationList.get(i).ColumnList.get(j).Type;
+				s3 = s.RelationList.get(i).PrimaryDetails.Type;
+				s2 = s.TrackType.get(s2).toString();
+				s3 = s.TrackType.get(s3).toString();
+				s4.add(s3);
+				seed_file.ColGenerator(s1,s2,s4,"type2");
+				seed_file.ColGenerator(s1,s2,s4,"type3");
+				seed_file.ColGenerator(s1, s2, s4, "type4");
+				if((s.RelationList.get(i).ColumnList.get(j).VarType).equals("category")) {
+					seed_file.ColGenerator(s1,s2,s4,"type1");
+				}
+				
+                
+			}
+		}
+		
+	}
+	public void Synonym_Seed(Schema s, Seed seed_file, Predicate pred_file) {
+		String s1,s2,s3,s5;
+		ArrayList<String> s4 = new ArrayList<String>();
+		for(int i=0;i<s.RelationList.size();i++) {
+			for(int j=0;j<s.RelationList.get(i).ColumnList.size();j++) {
+				for(int k=0;k<s.RelationList.get(i).ColumnList.get(j).Synonym.size();k++) {
+					s4.clear();
+					s1 = s.RelationList.get(i).ColumnList.get(j).Synonym.get(k);
+					s2 = s.RelationList.get(i).ColumnList.get(j).Type;
+					s3 = s.RelationList.get(i).PrimaryDetails.Type;
+					s2 = s.TrackType.get(s2).toString();
+					s3 = s.TrackType.get(s3).toString();
+					s4.add(s3);
+					s5 = s.RelationList.get(i).ColumnList.get(j).Name;
+					//System.out.println(s4);
+					seed_file.synGenerator(s1,s5,s2,s4,"type2");
+					seed_file.synGenerator(s1,s5,s2,s4,"type3");
+					seed_file.synGenerator(s1,s5,s2,s4,"type4");
+					if((s.RelationList.get(i).ColumnList.get(j).VarType).equals("Category")) {
+						seed_file.synGenerator(s1,s5,s2,s4,"type1");
+					}
+				
+				}   
+			}
+		}
+		
+	}
+	public void Preposition_seed(Schema s, Seed seed_file, Predicate pred_file) {
+		String s1,s2,s3,s4,s5;
+		for(int i=0;i<s.RelationList.size();i++) {
+			for(int j=0;j<s.RelationList.get(i).ColumnList.size();j++) {
+				for(int k=0;k<s.RelationList.get(i).ColumnList.get(j).PrepositionList.size();k++) {
+					s1 = s.RelationList.get(i).ColumnList.get(j).PrepositionList.get(k).name;
+					s2 = s.TrackType.get(s.RelationList.get(i).PrimaryDetails.Type).toString();
+					s3 = s.TrackType.get(s.RelationList.get(i).ColumnList.get(j).Type).toString();
+					s4 = "pkey_retriever:<" + s3 + "," + s2 + ">";
+					s5 = "set_retriever:<" + s2 + ",<" + s3 + ",t>>";
+					seed_file.Preposition(s1,s4,s5,s2);
+					
+				}
+			}
+		}
+	}
+	
+	public void symbol_giver(Nodes x, Schema s) {
+		String name = x.name;
+		
+		if(s.TrackType.get(name)!=null){
+		    x.symbol = s.TrackType.get(name).toString();
+		}else{
+			//System.out.println(lastsymb);
+		    x.symbol = nextPerm(lastsymb);
+		    lastsymb = x.symbol;
+		    s.TrackType.put(name, lastsymb);
+		}
+		for(int i=0;i<x.children.size();i++) {
+			symbol_giver(x.children.get(i),s);
+		}
+	}
+
+	public void hGenerator(Nodes x, ArrayList<String> hierar_list) {
+		for(int i=0;i<x.children.size();i++) {
+			hierar_list.add("("+x.children.get(i).symbol+","+x.symbol+")"+"       // "+x.children.get(i).name);
+		}
+		for(int i=0;i<x.children.size();i++) {
+			hGenerator(x.children.get(i),hierar_list);
+		}
+	}
+
+	
+}
---
+++ Documentation/Code/Predicate.java
@@ -0,0 +1,362 @@
+package ccg;
+import java.util.*;
+
+class Predicate{
+    
+	Map<String, ArrayList<String>> pred_dict = new HashMap<String, ArrayList<String>>(); //For Seed File
+    ArrayList<String> predicate = new ArrayList<String>();
+    
+    public void PredGenerator(String name, String ctype, String ptype, String cat_type){
+        
+        String pred1 = name + ":";
+        String pred2 = name + ":";
+        String pred3 = name + ":";
+        String pred4 = name + ":";
+        
+        ArrayList<String> track_name = new ArrayList<String>();
+        
+        if(cat_type=="category") {
+        
+        	pred1 += "<" + ctype + "," + "t" + ">";
+            this.predicate.add(pred1);
+            
+            
+            pred2 = name + ":";
+            for(int i=0;i<1;i++)
+            
+                pred2 = pred2 + "<" + ptype + ",";  
+            
+            pred2 = pred2 + ctype;
+            for(int i=0;i<1;i++){
+                pred2 = pred2 + ">";}
+            
+            this.predicate.add(pred2);
+            
+            
+            int p_size = 1;
+            pred3 = name + ":";
+            for(int i=0; i<p_size; i++) {
+            
+                String s = "<" + ptype + ",";
+                pred3 += s;
+            }
+            
+            pred3 += "<" + ctype + ",t" + ">";
+            for(int i=0; i<p_size; i++) {
+            
+                pred3 += ">";
+            }
+            
+            this.predicate.add(pred3);
+            pred4 = name + ":" + "<" + ptype + ",<" + "t," + ctype + ">>";
+            this.predicate.add(pred4);
+            track_name.add(pred1);
+            track_name.add(pred2);
+            track_name.add(pred3);
+            track_name.add(pred4);
+            
+            pred_dict.put(name, track_name);
+        }
+        else
+        {            
+            pred2 = name + ":";
+            for(int i=0;i<1;i++)
+            
+                pred2 = pred2 + "<" + ptype + ",";  
+            
+            pred2 = pred2 + ctype;
+            for(int i=0;i<1;i++){
+                pred2 = pred2 + ">";
+            }
+            
+            this.predicate.add(pred2);
+            
+            
+            int p_size = 1;
+            pred3 = name + ":";
+            for(int i=0; i<p_size; i++) {
+            
+                String s = "<" + ptype + ",";
+                pred3 += s;
+            }
+            
+            pred3 += "<" + ctype + ",t" + ">";
+            for(int i=0; i<p_size; i++) {
+            
+                pred3 += ">";
+            }
+            this.predicate.add(pred3);
+            pred4 = name + ":" + "<" + ptype + ",<" + "t," + ctype + ">>";
+            this.predicate.add(pred4);
+            
+            track_name.add(pred2);
+            track_name.add(pred3);
+            track_name.add(pred4);
+            
+            pred_dict.put(name, track_name);
+        
+        }
+
+    }
+}
+
+class Constant{
+    
+    ArrayList<String> consts = new ArrayList<String>();
+    
+    public void ConstGenerator(String value, String c_type) {
+    
+    	String str = value + ":" + c_type;
+    	this.consts.add(str);
+    
+    }
+}
+
+class NpList{
+    
+    ArrayList<String> np_list = new ArrayList<String>();
+    ArrayList<String> np_list1 = new ArrayList<String>();
+    
+    public void NpGenerator(String value, String c_type, String col_value, String pos_tag, String ptype) {
+    
+		String str = value + " :- " + pos_tag + " : " + col_value + ":" + c_type;
+		this.np_list.add(str);
+		str = "";
+		str = value + " :- " + pos_tag + " : " + "(pkey_retriever:<" + c_type + "," + ptype + "> " + col_value + ":" + c_type + ")";
+		this.np_list1.add(str);
+    }
+}
+	
+class Seed{
+    
+    ArrayList<String> seed_verb = new ArrayList();
+    ArrayList<String> seed_col = new ArrayList();
+    
+    public void VerbGenerator(String verb_name, String verb_type, String col_name, String col_pred1, String col_pred2){
+        
+        switch(verb_type) {
+        
+            case "intransitive":
+                break;
+                
+            case "transitive":
+                String str = verb_name + " :- " + "(S\\NP)/NP" + " : " + "(lambda $0:e (lambda $1:e (" + col_pred2 + " $0 $1)))";
+                seed_verb.add(str);
+                str = verb_name + " :- " + "(S\\NP)/NP" + " : " + "(lambda $0:e (lambda $1:e (" + col_pred2 + " $1 $0)))";
+                seed_verb.add(str);
+                str = verb_name + " :- " + "NP/NP" + " : " + "(lambda $0:e (" + col_pred1 + " $0))";
+                seed_verb.add(str);
+                
+                break;
+                
+            case "ditransitive":
+                break;
+        }
+    }
+    
+    public void ColGenerator(String name, String ctype, ArrayList<String> ptype, String pred_type){
+        
+        String str="", pred=name + ":";
+        switch(pred_type) {
+        
+            // state -> (S/(S\NP)) ??????????????????
+            case "type1":
+                pred += "<" + ctype + "," + "t" + ">";
+                str = name + " :- " + "N" + " : " + pred;
+                seed_col.add(str);
+                break;
+                
+            case "type2":
+                pred = name + ":";
+                for(int i=0;i<ptype.size();i++) {
+                
+                    pred = pred + "<" + ptype.get(i) + ",";  
+                }
+                
+                pred = pred + ctype;
+                for(int i=0;i<ptype.size();i++){
+                    pred = pred + ">";
+                }
+                
+                str = name + " :- " + "NP/NP" + " : " + "(lambda $0:e (" + pred + " $0))";
+                seed_col.add(str);
+                str = name + " :- " + "S/PP" + " : " + "(lambda $0:e (" + pred + " $0))";
+                seed_col.add(str);
+                break;
+            
+            // density -> N, S/NP   <lo,i> ????????????????????????????
+            case "type3":
+                int p_size = ptype.size();
+                pred = name + ":";
+                for(int i=0; i<p_size; i++) {
+                
+                    String s = "<" + ptype.get(i) + ",";
+                    pred += s;
+                }
+                pred += "<" + ctype + ",t" + ">";
+                for(int i=0; i<p_size; i++) {
+                
+                    pred += ">";
+                }
+                
+                str = name + " :- " + "(S\\NP)/NP" + " : " + "(lambda $0:e (lambda $1:e (" + pred + " $1 $0)))";
+                seed_col.add(str);
+                break;
+            case "type4":
+            	
+                pred = name + ":";
+                for(int i=0; i<ptype.size(); i++) {
+                
+                    String s = "<" + ptype.get(i) + ",";
+                    pred += s;
+                }
+                pred += "<t," + ctype + ">";
+                for(int i=0; i<ptype.size(); i++) {
+                    
+                    pred += ">";
+                }
+            	str = name + " :- " + "NP/PP : " + "(lambda $0:<e,t> (lambda $1:e (" + pred + " $1 " + "($0 $1))))";   
+            	seed_col.add(str);
+            	str = name + " :- " + "NP/(S\\NP) : " + "(lambda $0:<e,t> (lambda $1:e (" + pred + " $1 " + "($0 $1))))";
+            	seed_col.add(str);
+         }
+    }
+public void synGenerator(String name, String col, String ctype, ArrayList<String> ptype, String pred_type){
+        
+        String str="", pred=name + ":";
+        switch(pred_type) {
+        
+            // state -> (S/(S\NP)) ??????????????????
+            case "type1":
+                pred += "<" + ctype + "," + "t" + ">";
+                str = name + " :- " + "N" + " : " + pred;
+                seed_col.add(str);
+                break;
+                
+            case "type2":
+                pred = col + ":";
+                for(int i=0;i<ptype.size();i++) {
+                
+                    pred = pred + "<" + ptype.get(i) + ",";  
+                }
+                
+                pred = pred + ctype;
+                for(int i=0;i<ptype.size();i++){
+                    pred = pred + ">";
+                }
+                
+                str = name + " :- " + "NP/NP" + " : " + "(lambda $0:e (" + pred + " $0))";
+                seed_col.add(str);
+                str = name + " :- " + "S/PP" + " : " + "(lambda $0:e (" + pred + " $0))";
+                seed_col.add(str);
+                str = name + " :- " + "N" + " : " + "(lambda $0:e (" + pred + " $0))";
+                seed_col.add(str);
+                break;
+            
+            // density -> N, S/NP   <lo,i> ????????????????????????????
+            case "type3":
+                int p_size = ptype.size();
+                pred = col + ":";
+                for(int i=0; i<p_size; i++) {
+                
+                    String s = "<" + ptype.get(i) + ",";
+                    pred += s;
+                }
+                pred += "<" + ctype + ",t" + ">";
+                for(int i=0; i<p_size; i++) {
+                
+                    pred += ">";
+                }
+                
+                str = name + " :- " + "(S\\NP)/NP" + " : " + "(lambda $0:e (lambda $1:e (" + pred + " $1 $0)))";
+                seed_col.add(str);
+                break;
+            case "type4":
+            	
+                pred = col + ":";
+                for(int i=0; i<ptype.size(); i++) {
+                
+                    String s = "<" + ptype.get(i) + ",";
+                    pred += s;
+                }
+                pred += "<t," + ctype + ">";
+                for(int i=0; i<ptype.size(); i++) {
+                    
+                    pred += ">";
+                }
+            	str = name + " :- " + "NP/PP : " + "(lambda $0:<e,t> (lambda $1:e (" + pred + " $1 " + "($0 $1))))";   
+            	seed_col.add(str);
+            	str = name + " :- " + "NP/(S\\NP) : " + "(lambda $0:<e,t> (lambda $1:e (" + pred + " $1 " + "($0 $1))))";
+            	seed_col.add(str);
+         }
+    }
+    public ArrayList<String> seed_adj = new ArrayList<String>(); 
+    public void Adjectives(String adj, String level, String pred, String greater, String max){
+        String entry = "";
+        if(level.equals("0")){
+            entry += adj + " :- NP/NP : (lambda $0:e (" + pred + " $0))";
+            //System.out.println(entry);
+        }else if(level.equals("1")){
+            entry += adj + " :- PP/NP : (lambda $0:e (lambda $1:e (" + greater + " (" + pred + " $1) (" + pred + " $0))))";
+            //System.out.println(entry);
+        }else{
+            entry += adj + " :- NP/N : (lambda $0:<e,t> (" + max + " $0 " + pred + "))";
+            //System.out.println(entry);
+        }
+        seed_adj.add(entry);
+    }
+
+    public void Adjectives(String adj, String level, String greater, String max){
+        String entry = "";
+        if(level.equals("1")){
+        	entry += adj + " :- (PP/NP)/NP : (lambda $0:e (lambda $1:<e,i> (lambda $2:e (" + greater + " (" + "$1" + " $2) (" + "$1" + " $0)))))";
+            //System.out.println(entry);
+        	seed_adj.add(entry);
+        }else if(level.equals("2")){
+            entry += adj + " :- (NP/N)/N : (lambda $0:<e,e> (lambda $1:<e,t> (" + max + " $1 $0" + ")))";
+            seed_adj.add(entry);
+            //System.out.println(entry);
+            entry = adj + " :- (NP/N)/N : (lambda $0:<e,t> (lambda $1:<e,e> (" + max + " $0 $1" + ")))";
+            seed_adj.add(entry);
+            //System.out.println(entry);
+        }
+    }
+        
+    
+    public ArrayList<String> seed_prep = new ArrayList<String>();
+    public void Preposition(String prep, String pred1, String pred2, String ptype) {
+    	String entry = "";
+    	entry += prep + " :- PP/NP : (lambda $0:e (" + pred1 + " $0))";//returns pkey, may be not required
+    	seed_prep.add(entry);
+		entry = "";
+		entry += prep + " :- PP/NP : (lambda $0:<e,e> (lambda $1:e (" + pred1 + " ($0 $1))))";
+    	seed_prep.add(entry);
+    	entry = "";
+        entry += prep + " :- PP/NP : (lambda $0:e (lambda $1:"+ ptype + " (" + pred2 + " $1 $0)))";//here type 4 will be used
+        entry = "";
+        entry += prep + " :- PP/NP : (lambda $0:<e,e> (lambda $1:"+ ptype + " (lambda $2:e (" + pred2 + " $1 ($0 $2)))))";
+        seed_prep.add(entry);
+    }
+    
+
+	
+		public ArrayList<String> complete_seed = new ArrayList<String>();
+    	public void comseed(){
+		complete_seed.addAll(this.seed_verb);
+		complete_seed.addAll(this.seed_col);
+		complete_seed.addAll(this.seed_adj);
+		complete_seed.addAll(this.seed_prep);
+		
+		
+	}
+}
+
+class Hierar{
+	public ArrayList<String> hierar_list = new ArrayList<String>();
+	
+	public static String CommonAncestor(String a, String b){
+	
+		return "Nikunj-C";
+	
+	}
+}
